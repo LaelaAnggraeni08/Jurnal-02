7fe77eaba1c226afdbed23e627b57f4ecd7a1e40
LaelaAnggraeni08 <anggraenilaila64@gmail.com>
2021-09-27 21:26:03 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama : Laela");
+		System.out.println("NIM  : 6706213111");
+		System.out.println("Kelas: 4503");
+	}
+}	
